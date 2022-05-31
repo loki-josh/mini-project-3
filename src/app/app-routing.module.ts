c3198cf55c8bb41d008069f4f213ba66b157c267
loki-josh <lokijosh19@gmail.com>
2022-05-31 10:53:20 +0530

    changes done with respect to rxjs assignment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterChildComponent } from './main/footer/footer-child/footer-child.component';
 import { FooterComponent } from './main/footer/footer.component';
 import { HeaderComponent } from './main/header/header.component';
 import { MainComponent } from './main/main/main.component';
+import { AuthChild2Guard } from './shared/auth-child-2.guard';
 import { AuthGuard } from './shared/auth.guard';
 import { LoginComponent } from './users/login/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
@@ -13,10 +15,18 @@ const routes: Routes = [
   {path:'main', component:MainComponent},
   {path:'header', component:HeaderComponent},
   {path:'footer', component:FooterComponent, canActivate:[AuthGuard]},
+  // {
+  //   path: 'footer', 
+  //   loadChildren: () => import('./main/footer/footer.module').then(m => m.FooterModule)
+  // },
   {
-    path: 'footer',
-    loadChildren: () => import('./main/footer/footer.module').then(m => m.FooterModule)
+    path:'footer', 
+    component: FooterComponent, canActivateChild:[AuthChild2Guard],
+    children: [
+      {path:'footer1', component: FooterChildComponent,}
+    ]
   }
+  
 
 
 
